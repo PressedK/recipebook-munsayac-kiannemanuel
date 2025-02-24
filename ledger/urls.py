d0681d5fad0b20ea22135cbf1d6f51d9a2281314
PressedK <lightning.mckey@yahoo.com>
2025-02-24 23:00:14 +0800

    Created Pages for Recipe 1 and Recipe 2
    Self-Explanatory

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index, recipes_list#every new page gets added here
+from .views import index, recipes_list, recipe1, recipe2 #every new page gets added here
 urlpatterns = [
     path('', index, name='index'),
     path('recipes/list', recipes_list, name='recipes_list'),
+    path('recipe/1', recipe1, name='recipe1'),
+    path('recipe/2', recipe2, name='recipe2'),
 ]
 
 app_name = "ledger"
