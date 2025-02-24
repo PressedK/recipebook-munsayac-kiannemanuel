cfdeee7193dd4c0fbbae774f703988ca243de47e
PressedK <lightning.mckey@yahoo.com>
2025-02-24 22:35:38 +0800

    Created Recipe Home Page
    1. added context in views.py
    2. Created the recipes/list page
    3. Iterated through the recipe names in the context, and allowed them to be hyperlinked
    4. debugged url

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, recipes_list#every new page gets added here
 urlpatterns = [
     path('', index, name='index'),
+    path('recipes/list', recipes_list, name='recipes_list'),
 ]
 
 app_name = "ledger"
