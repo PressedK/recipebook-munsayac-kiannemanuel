0271500de6c2a624736fc559a00450aaa1fb7809
PressedK <lightning.mckey@yahoo.com>
2025-03-09 21:02:52 +0800

    updated admin and models
    admin now includes recipeingredient
    changes made towards arguments in models

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient 
+from .models import Recipe, Ingredient, RecipeIngredient
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
@@ -7,5 +7,9 @@ class RecipeAdmin(admin.ModelAdmin):
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
 
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    model = RecipeIngredient
+
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
--- ledger/models.py
+++ ledger/models.py
@@ -9,7 +9,7 @@ class Ingredient(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse("ingredient_name", args=[self.name])
+        return reverse("ingredient_name", args=[str(self.id)])
 
 class Recipe(models.Model):
     name = models.CharField(max_length = 255)
@@ -18,12 +18,12 @@ class Recipe(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse("recipe_name", args=[str(self.name)])
+        return reverse("recipe_name", args=[str(self.id)])
 
 class RecipeIngredient(models.Model):
-    Quantity = models.IntegerField()
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name = 'ingredients')
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE,  related_name = 'recipe')
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name = 'ingredients')
+    Quantity = models.CharField(max_length = 255)
 
 
 
