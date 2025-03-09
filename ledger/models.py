3ab129ceecd7879e25a95a468f08d856c8bf7c79
PressedK <lightning.mckey@yahoo.com>
2025-03-09 17:42:52 +0800

    Created the Ingredient, Recipe, and RecipeIngredient Models
    finished the ingredient and recipe models. Specifically, added a name parameter, and allowed the model to return a name, and access the absolute url.
    
    Started RecipeIngredient

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Models)
+    name = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("ingredient_name", args=[self.name])
+
+class Recipe(models.Models)
+    name = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("recipe_name", args=[self.name])
+
+class RecipeIngredient(models.Models)
+    Quantity = models.CharField(max_length = 255)
+    
+
+
+
