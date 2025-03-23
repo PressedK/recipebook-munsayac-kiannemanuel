11d9ef3086a9a4fb83c7e8e5d83203d817736467
PressedK <lightning.mckey@yahoo.com>
2025-03-23 18:53:11 +0800

    Created Profile Model, Admin and Edited Recipe Model
    created profile models and edited recipe model to have new inputs

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient, RecipeIngredient
+from .models import Recipe, Ingredient, RecipeIngredient, Profile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
@@ -19,6 +21,13 @@ class RecipeIngredientAdmin(admin.ModelAdmin):
     list_display = ('recipe', 'ingredient', 'Quantity' )
     # Enable filtering via the teacher's name and the number of units
     list_filter = ('recipe', 'ingredient', 'Quantity' )
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+class UserAdmin(admin.BaseUserAdmin):
+    inlines = [ProfileInline,]
 
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
--- ledger/models.py
+++ ledger/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
 # Create your models here.
 
 class Ingredient(models.Model):
@@ -10,6 +12,9 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length = 255)
+    author = models.CharField(max_length = 255)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -22,4 +27,10 @@ class RecipeIngredient(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name = 'recipe')
     Quantity = models.CharField(max_length = 255)
 
-    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 50)
+    bio = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
